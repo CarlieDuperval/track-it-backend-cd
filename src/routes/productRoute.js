72bcbfc7fbdb96151341eee95c95ebdf692d8aff
Carlie Duperval <carlierichell@gmail.com>
2022-06-04 00:27:32 -0400

    Added Getby Filer  to ProductRoutes

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -3,29 +3,69 @@ import {
   createProduct,
   getAllProducts,
   getProduct,
+  getProductByFilter,
+  getProductById,
   updateProduct,
 } from "../services/productService.js";
 
 export const productRouter = Router();
 
-productRouter.post("/product", async (req, res) => {
-  const product = req.body;
-  const id = await createProduct(product);
-  res.status(200).send(id.toString);
+productRouter.post("/products", async (req, res) => {
+  try {
+    const product = req.body;
+    const id = await createProduct(product);
+    res.status(200).send(id.toString);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-productRouter.get("/product", async (req, res) => {
-  const productList = await getAllProducts();
-  res.status(200).send(productList);
+productRouter.get("/products", async (req, res) => {
+  try {
+    const productList = await getAllProducts();
+    res.status(200).send(productList);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-productRouter.get("/product/:name", async (res, req) => {
-  const { name } = req.params;
-  const product = await getProduct(name);
-  res.status(200).send(product);
+productRouter.get("/products/:name", async (res, req) => {
+  try {
+    const { name } = req.params;
+    const product = await getProduct(name);
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-productRouter.patch("/product/:name", async (res, req) => {
-  const { name, updateObj } = await updateProduct(name, updateObj);
-  res.status(200).send("Product updated");
+productRouter.patch("/products/:name", async (res, req) => {
+  try {
+    const { name, updateObj } = await updateProduct(name, updateObj);
+    res.status(200).send("Product updated");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+productRouter.get("/products/:id", async (res, req) => {
+  try {
+    const { id } = req.params;
+    const col = await getProductById(id);
+    res.status(200).send(col);
+  } catch (error) {
+    res.send(500).send(error);
+  }
+});
+
+productRouter.get("/products", async (res, req) => {
+  const { name, productCategory, cost, salePrice, date } = req.quey;
+  const filter = { name, productCategory, cost, salePrice, date };
+  try {
+    const col = await getProductByFilter(filter);
+    res.status(200).send(col);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 });
