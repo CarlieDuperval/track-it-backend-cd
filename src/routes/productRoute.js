964dfdd9d7830d6a1653daf79d7e288c823dba2e
Carlie Duperval <carlierichell@gmail.com>
2022-06-03 23:11:07 -0400

    Added porudct as route for the CRU

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -8,24 +8,24 @@ import {
 
 export const productRouter = Router();
 
-productRouter.post("/sales", async (req, res) => {
+productRouter.post("/product", async (req, res) => {
   const product = req.body;
   const id = await createProduct(product);
   res.status(200).send(id.toString);
 });
 
-productRouter.get("/sales", async (req, res) => {
+productRouter.get("/product", async (req, res) => {
   const productList = await getAllProducts();
   res.status(200).send(productList);
 });
 
-productRouter.get("/sales/:name", async (res, req) => {
+productRouter.get("/product/:name", async (res, req) => {
   const { name } = req.params;
   const product = await getProduct(name);
   res.status(200).send(product);
 });
 
-productRouter.patch("/sales/:name", async (res, req) => {
+productRouter.patch("/product/:name", async (res, req) => {
   const { name, updateObj } = await updateProduct(name, updateObj);
   res.status(200).send("Product updated");
 });
