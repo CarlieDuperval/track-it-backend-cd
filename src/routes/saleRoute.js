9acb42857e8ee0dd6ceaf53702f9e92fe17563a1
Carlie Duperval <carlierichell@gmail.com>
2022-06-03 23:19:46 -0400

    Added .post to salesRouts

---
+++ src/routes/saleRoute.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import { createSales } from "../services/salesService.js";
+export const salesRouter = Router();
+
+salesRouter.post("/sales", async (req, res) => {
+  const sales = req.body;
+  const idSales = await createSales(sales);
+  res.status(200).send(idSales.toString);
+});
